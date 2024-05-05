e9941a3cae1687e02a9f45d74f0f97e07c4b13e0
Arik Chakma <arikchangma@gmail.com>
2024-05-05 10:02:26 +0600

    feat: add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   build: {
@@ -14,4 +16,6 @@ export default defineConfig({
     },
   },
   integrations: [tailwind(), react(), mdx()],
+  output: 'static',
+  adapter: vercel(),
 });
