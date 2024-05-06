08d5b335c95289a3bae7d4c85972b16b01b32f81
Arik Chakma <arikchangma@gmail.com>
2024-05-06 07:51:14 +0600

    feat: add images

--- src/components/DocsHeader.tsx
+++ src/components/DocsHeader.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import type { CollectionEntry } from 'astro:content';
+import { ArrowUpRight, Github } from 'lucide-react';
 
 type DocsHeaderProps = {
   guides: CollectionEntry<'guides'>[];
@@ -23,8 +24,18 @@ export function DocsHeader(props: DocsHeaderProps) {
           </a>
         </div>
         <div className="flex items-center pr-12">
+          <a
+            href="https://github.com/arikchakma/mly.fyi"
+            target="_blank"
+            className="flex h-[38px] w-[38px] items-center justify-center rounded-md hover:bg-gray-100"
+          >
+            <Github size="15" />
+            <span className="sr-only">GitHub</span>
+          </a>
+
           <button
             onClick={() => setIsActive(!isActive)}
+            aria-label={isActive ? 'Close Menu' : 'Open Menu'}
             className="-mr-[12px] rounded-md p-[12px] hover:bg-gray-100"
           >
             <img
@@ -50,6 +61,15 @@ export function DocsHeader(props: DocsHeaderProps) {
           );
         })}
       </div>
+
+      <a
+        className="flex items-center justify-center gap-1.5 bg-orange-500 px-5 py-2 font-medium text-white hover:opacity-85"
+        href="https://go.mly.fyi/mly-demo"
+        target="_blank"
+      >
+        <ArrowUpRight size="16" className="stroke-[2.5px]" />
+        Check Live Demo
+      </a>
     </>
   );
 }
